6afbdba466e16f8a3a01d99347e895c9023a2279
Yunus Kocabay <truewd@hotmail.com>
2024-03-25 02:29:57 +0300

    First Commit

--- 2/build/assets/avatar-5-641be5a8.js
+++
@@ -1,1 +0,0 @@
-const a="/build/images/avatar-1.jpg",t="/build/images/avatar-2.jpg",s="/build/images/avatar-3.jpg",r="/build/images/avatar-4.jpg",g="/build/images/avatar-5.jpg";export{a,t as b,s as c,r as d,g as e};
